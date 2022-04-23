0e6c13a7f5febd91575612e8d342fdcccd71e8f6
Joseph-GM <jkim.english75@gmail.com>
2022-04-23 10:45:14 +0900

    add comment

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,8 @@
-const toggle = document.querySelector('.navbar_toggleBtn');
+const toggle = document.querySelector('.navbar_toggleBtn'); //class node 중 navbar_toggleBtn 지정
 const menu = document.querySelector('.navbar_menu');
 const icons = document.querySelector('.navbar_icon');
 
 toggle.addEventListener('click', ()=>{
-    menu.classList.toggle('active');
+    menu.classList.toggle('active'); // click했을 때 active가 없다면 active를 추가해 주고 있으면 active를 없애줌
     icons.classList.toggle('active');
 });
